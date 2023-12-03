ddaae940733c4f755c197a76700f52011fa32a2e
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-03 15:56:50 +1100

    feat: 3.1 prep

---
+++ questions/3.1/run.ts
@@ -0,0 +1,15 @@
+let file = Bun.file(Bun.resolveSync('./resources/games.txt', import.meta.dir))
+let text = await file.text()
+
+let input1 = text.split('\n')
+let input2 = [
+  'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+  'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+  'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+  'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+  'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+]
+
+const fn = () => {}
+
+console.log(fn())
