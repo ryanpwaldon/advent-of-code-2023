408b49d0ce420500805ef0068900dcf678c3dd53
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-06 16:54:09 +1100

    feat: day 6

--- questions/6.1/run.ts
+++ questions/6.1/run.ts
@@ -1,9 +1,32 @@
-let file = Bun.file(Bun.resolveSync('./resources/document.txt', import.meta.dir))
-let text = await file.text()
+let a = [
+  [7, 9],
+  [15, 40],
+  [30, 200],
+]
 
-let input1 = text.split('\n')
-let input2 = ['']
+let b = [
+  [53, 275],
+  [71, 1181],
+  [78, 1215],
+  [80, 1524],
+]
 
-const main = (lines: string[]) => {}
+const main = (results: number[][]) => {
+  let possibilities = []
+  for (let [time, distance] of results) {
+    let count = 0
+    for (let i = 0; i <= time; i++) {
+      let timeLeft = time - i
+      let dist = i * timeLeft
+      if (dist > distance) count++
+    }
+    possibilities.push(count)
+  }
+  return possibilities.reduce((p, val) => p * val, 1)
+}
 
-console.log(main(input2))
+console.log(main(b))
+
+// time allowed for each race
+// best distance ever recorded in that race
+// go farther
--- questions/6.2/run.ts
+++ questions/6.2/run.ts
@@ -1,9 +1,26 @@
-let file = Bun.file(Bun.resolveSync('./resources/document.txt', import.meta.dir))
-let text = await file.text()
+let a = [71530, 940200]
+let b = [53717880, 275118112151524]
 
-let input1 = text.split('\n')
-let input2 = ['']
+const main = ([time, distance]: number[]) => {
+  let left = 0
+  let right = time
+  for (let i = 0; i <= time; i++) {
+    let timeLeft = time - i
+    let dist = i * timeLeft
+    if (dist > distance) {
+      left = i
+      break
+    }
+  }
+  for (let i = time; i >= 0; i--) {
+    let timeLeft = time - i
+    let dist = i * timeLeft
+    if (dist > distance) {
+      right = i
+      break
+    }
+  }
+  return right - left + 1
+}
 
-const main = (lines: string[]) => {}
-
-console.log(main(input2))
+console.log(main(b))
