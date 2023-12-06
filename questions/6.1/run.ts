cab6d0fa7ee9a1ed1357fed059ea40c6a57563b5
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-06 15:53:15 +1100

    feat: day 6 prep

---
+++ questions/6.1/run.ts
@@ -0,0 +1,9 @@
+let file = Bun.file(Bun.resolveSync('./resources/document.txt', import.meta.dir))
+let text = await file.text()
+
+let input1 = text.split('\n')
+let input2 = ['']
+
+const main = (lines: string[]) => {}
+
+console.log(main(input2))
