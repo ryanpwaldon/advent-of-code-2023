bf73863586b72e10da84ed0262587437fd80daad
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-08 17:29:34 +1100

    feat: day 8

--- questions/8.1/run.ts
+++ questions/8.1/run.ts
@@ -4,15 +4,37 @@ let inputTest = await Bun.file(Bun.resolveSync('./resources/test.txt', import.me
 // Format
 const format = (text: string) => {
   let lines = text.split('\n')
-  return lines
+  let instructions = lines[0]
+  lines = lines.slice(2)
+  let linesArr = lines.map((line) => {
+    let node = line.split('=')[0].trim()
+    let [left, right] = line
+      .split('=')[1]
+      .trim()
+      .split(',')
+      .map((node) => node.replaceAll(/[\(\)]/g, '').trim())
+    return [node, left, right]
+  })
+  let list: Record<string, [string, string]> = {}
+  for (let [node, left, right] of linesArr) list[node] = [left, right]
+  return { instructions, list }
 }
 
 // Solve
 const solve = (input: string) => {
-  let lines = format(input)
-  return lines
+  let { instructions, list } = format(input)
+  let node = 'AAA'
+  let steps = 0
+  let i = 0
+  while (node !== 'ZZZ') {
+    let next = instructions[i] === 'L' ? 0 : 1
+    node = list[node][next]
+    i = i === instructions.length - 1 ? 0 : i + 1
+    steps++
+  }
+  return steps
 }
 
 // Run
-const ans = solve(inputTest)
+const ans = solve(inputMain)
 console.log(ans)
--- questions/8.2/run.ts
+++ questions/8.2/run.ts
@@ -1,18 +1,55 @@
-let inputMain = await Bun.file(Bun.resolveSync('./resources/main.txt', import.meta.dir)).text()
-let inputTest = await Bun.file(Bun.resolveSync('./resources/test.txt', import.meta.dir)).text()
+let input = await Bun.file(Bun.resolveSync('./resources/main.txt', import.meta.dir)).text()
 
 // Format
 const format = (text: string) => {
   let lines = text.split('\n')
-  return lines
+  let instructions = lines[0]
+  lines = lines.slice(2)
+  let linesArr = lines.map((line) => {
+    let node = line.split('=')[0].trim()
+    let [left, right] = line.split('=')[1].trim().split(',').map((node) => node.replaceAll(/[\(\)]/g, '').trim()) // prettier-ignore
+    return [node, left, right]
+  })
+  let starts = []
+  let list: Record<string, [string, string]> = {}
+  for (let [node, left, right] of linesArr) {
+    list[node] = [left, right]
+    if (node.endsWith('A')) starts.push(node)
+  }
+  return { starts, instructions, list }
+}
+
+function gcd(a: number, b: number): number {
+  return b === 0 ? a : gcd(b, a % b)
+}
+
+function lcm(a: number, b: number): number {
+  return Math.abs(a * b) / gcd(a, b)
+}
+
+function findLCM(numbers: number[]): number {
+  return numbers.reduce((acc, val) => lcm(acc, val), 1)
 }
 
 // Solve
 const solve = (input: string) => {
-  let lines = format(input)
-  return lines
+  let { starts, instructions, list } = format(input)
+  let nodes = [...starts]
+  let factors: number[] = []
+  for (let node of nodes) {
+    let steps = 0
+    let i = 0
+    while (!node.endsWith('Z')) {
+      let next = instructions[i] === 'L' ? 0 : 1
+      node = list[node][next]
+      i = i === instructions.length - 1 ? 0 : i + 1
+      steps++
+    }
+    factors.push(steps)
+  }
+  return findLCM(factors)
 }
 
 // Run
-const ans = solve(inputTest)
+const ans = solve(input)
 console.log(ans)
