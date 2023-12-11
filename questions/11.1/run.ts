b11bd512525f7b4b6c4840b4ea87ae0821a84000
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-11 16:26:11 +1100

    feat: day 11 🥊

--- questions/11.1/run.ts
+++ questions/11.1/run.ts
@@ -1,13 +1,52 @@
 let input = await Bun.file(Bun.resolveSync('./resources/input.txt', import.meta.dir)).text()
 
 const format = (text: string) => {
-  let lines = text.split('\n')
-  return lines
+  let rows = text.split('\n')
+  return rows.map((row) => row.split(''))
 }
 
 const solve = (input: string) => {
-  const lines = format(input)
-  return lines
+  let grid = format(input)
+  let rCount = grid.length
+  let cCount = grid[0].length
+  for (let r = 0; r < rCount; r++) {
+    if (grid[r].every((cell) => cell === '.')) {
+      grid.splice(r, 0, [...grid[r]])
+      rCount++
+      r++
+    }
+  }
+  for (let c = 0; c < cCount; c++) {
+    let empty = true
+    for (let r = 0; r < rCount; r++) {
+      if (grid[r][c] === '#') {
+        empty = false
+        break
+      }
+    }
+    if (empty) {
+      for (let r = 0; r < rCount; r++) {
+        grid[r].splice(c, 0, '.')
+      }
+      cCount++
+      c++
+    }
+  }
+  let points = []
+  for (let r = 0; r < rCount; r++) {
+    for (let c = 0; c < cCount; c++) {
+      if (grid[r][c] === '#') points.push([r, c])
+    }
+  }
+  let dist = 0
+  for (let i = 0; i < points.length - 1; i++) {
+    let [x1, y1] = points[i]
+    for (let j = i + 1; j < points.length; j++) {
+      let [x2, y2] = points[j]
+      dist += Math.abs(x1 - x2) + Math.abs(y1 - y2)
+    }
+  }
+  return dist
 }
 
 const ans = solve(input)
--- questions/11.2/run.ts
+++ questions/11.2/run.ts
@@ -1,13 +1,48 @@
 let input = await Bun.file(Bun.resolveSync('./resources/input.txt', import.meta.dir)).text()
 
 const format = (text: string) => {
-  let lines = text.split('\n')
-  return lines
+  let rows = text.split('\n')
+  return rows.map((row) => row.split(''))
 }
 
 const solve = (input: string) => {
-  const lines = format(input)
-  return lines
+  let grid = format(input)
+  let rCount = grid.length
+  let cCount = grid[0].length
+  let gapRows = new Set()
+  let gapCols = new Set()
+  for (let r = 0; r < rCount; r++) if (grid[r].every((cell) => cell === '.')) gapRows.add(r)
+  for (let c = 0; c < cCount; c++) {
+    let empty = true
+    for (let r = 0; r < rCount; r++) {
+      if (grid[r][c] === '#') {
+        empty = false
+        break
+      }
+    }
+    if (empty) gapCols.add(c)
+  }
+  let points = []
+  let gr = 0
+  for (let r = 0; r < rCount; r++) {
+    if (gapRows.has(r)) gr++
+    let gc = 0
+    for (let c = 0; c < cCount; c++) {
+      if (gapCols.has(c)) gc++
+      if (grid[r][c] === '#') {
+        points.push([r + 1000000 * gr - gr, c + 1000000 * gc - gc])
+      }
+    }
+  }
+  let dist = 0
+  for (let i = 0; i < points.length - 1; i++) {
+    let [x1, y1] = points[i]
+    for (let j = i + 1; j < points.length; j++) {
+      let [x2, y2] = points[j]
+      dist += Math.abs(x1 - x2) + Math.abs(y1 - y2)
+    }
+  }
+  return dist
 }
 
 const ans = solve(input)
