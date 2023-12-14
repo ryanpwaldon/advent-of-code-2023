4fd53c2fb9904a5c79ba4910729abdee2bf5eff1
Ryan Waldon <ryanpwaldon@gmail.com>
2023-12-14 16:45:14 +1100

    feat: day 14 ✨

--- questions/14.1/run.ts
+++ questions/14.1/run.ts
@@ -1,13 +1,40 @@
 let input = await Bun.file(Bun.resolveSync('./input.txt', import.meta.dir)).text()
 
 const format = (text: string) => {
-  let lines = text.split('\n')
+  let lines = text.split('\n').map((row) => row.split(''))
   return lines
 }
 
 const solve = (input: string) => {
-  let lines = format(input)
-  return lines
+  let grid = format(input)
+  let rCount = grid.length
+  let cCount = grid[0].length
+
+  let roll = (r: number, c: number) => {
+    if (grid[r][c] !== 'O') return
+    while (r > 0 && grid[r - 1][c] === '.') {
+      grid[r][c] = '.'
+      grid[r - 1][c] = 'O'
+      r--
+    }
+  }
+
+  for (let r = 0; r < rCount; r++) {
+    for (let c = 0; c < cCount; c++) {
+      roll(r, c)
+    }
+  }
+
+  let count = 0
+  for (let r = 0; r < rCount; r++) {
+    for (let c = 0; c < cCount; c++) {
+      if (grid[r][c] === 'O') {
+        count += rCount - r
+      }
+    }
+  }
+
+  return count
 }
 
 const ans = solve(input)
--- questions/14.2/run.ts
+++ questions/14.2/run.ts
@@ -1,14 +1,99 @@
 let input = await Bun.file(Bun.resolveSync('./input.txt', import.meta.dir)).text()
 
 const format = (text: string) => {
-  let lines = text.split('\n')
+  let lines = text.split('\n').map((row) => row.split(''))
   return lines
 }
 
 const solve = (input: string) => {
-  let lines = format(input)
-  return lines
+  let grid = format(input)
+  let rCount = grid.length
+  let cCount = grid[0].length
+
+  let rollNorth = (r: number, c: number) => {
+    if (grid[r][c] !== 'O') return
+    while (r > 0 && grid[r - 1][c] === '.') {
+      grid[r][c] = '.'
+      grid[r - 1][c] = 'O'
+      r--
+    }
+  }
+
+  let rollSouth = (r: number, c: number) => {
+    if (grid[r][c] !== 'O') return
+    while (r < rCount - 1 && grid[r + 1][c] === '.') {
+      grid[r][c] = '.'
+      grid[r + 1][c] = 'O'
+      r++
+    }
+  }
+
+  let rollWest = (r: number, c: number) => {
+    if (grid[r][c] !== 'O') return
+    while (c > 0 && grid[r][c - 1] === '.') {
+      grid[r][c] = '.'
+      grid[r][c - 1] = 'O'
+      c--
+    }
+  }
+
+  let rollEast = (r: number, c: number) => {
+    if (grid[r][c] !== 'O') return
+    while (c < cCount - 1 && grid[r][c + 1] === '.') {
+      grid[r][c] = '.'
+      grid[r][c + 1] = 'O'
+      c++
+    }
+  }
+
+  let roll = () => {
+    for (let r = 0; r < rCount; r++) {
+      for (let c = 0; c < cCount; c++) {
+        rollNorth(r, c)
+      }
+    }
+
+    for (let c = 0; c < cCount; c++) {
+      for (let r = 0; r < rCount; r++) {
+        rollWest(r, c)
+      }
+    }
+
+    for (let r = rCount - 1; r >= 0; r--) {
+      for (let c = 0; c < cCount; c++) {
+        rollSouth(r, c)
+      }
+    }
+
+    for (let c = cCount - 1; c >= 0; c--) {
+      for (let r = 0; r < rCount; r++) {
+        rollEast(r, c)
+      }
+    }
+  }
+
+  const getCount = () => {
+    let count = 0
+    for (let r = 0; r < rCount; r++) {
+      for (let c = 0; c < cCount; c++) {
+        if (grid[r][c] === 'O') {
+          count += rCount - r
+        }
+      }
+    }
+    return count
+  }
+
+  for (let i = 1; i <= 10000; i++) {
+    roll()
+    console.log(getCount())
+  }
+
+  return null
 }
 
-const ans = solve(input)
-console.log(ans)
+solve(input)
+
+// cycle start: 137
+// cycle size: 72
+// answer = (1000000000 - 137) % 72
